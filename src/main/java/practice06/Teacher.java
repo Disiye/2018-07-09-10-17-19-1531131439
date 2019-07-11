06ddd11eb7be3a2e66b92076e4752b03719e55be
WENGEM <emilio.wang@oocl.com>
2019-07-11 19:40:46 +0800

    complete Practice06Test

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass = 0;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass(){
+        return this.klass;
+    }
+
+    public String introduce(){
+        String introduce = null;
+        if(this.klass == 0){
+            introduce = "No Class";
+        }else {
+            introduce = "Class " + this.klass;
+        }
+        return super.introduce() + " I am a Teacher. I teach " + introduce + ".";
+    }
 }
