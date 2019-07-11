4d291a664486dd5490d6136d03c3e7b266139049
WENGEM <emilio.wang@oocl.com>
2019-07-11 19:49:21 +0800

    [Practice06Test] Refactor the code

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,28 +1,18 @@
 package practice06;
 
-public class Student {
-    private String name;
-    private int age;
+public class Student extends Person {
     private int klass;
+
     public Student(String name, int age, int klass) {
-        this.age = age;
+        super(name, age);
         this.klass = klass;
-        this.name = name;
     }
 
-    public String getName() {
-        return name;
+    public int getKlass(){
+        return this.klass;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public int getKlass() {
-        return klass;
-    }
-
-    public String introduce() {
-        return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
     }
 }
