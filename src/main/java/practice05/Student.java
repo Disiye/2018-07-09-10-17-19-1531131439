e2a877ac95d0cfc14740ca625bf8eb0004c14258
WENGEM <emilio.wang@oocl.com>
2019-07-11 19:20:12 +0800

    complete Practice05Test

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,28 @@
 package practice05;
 
 public class Student {
+    private String name;
+    private int age;
+    private int klass;
+    public Student(String name, int age, int klass) {
+        this.age = age;
+        this.klass = klass;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+    }
 }
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,22 @@
 package practice05;
 
 public class Worker {
+    private String name;
+    private int age;
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is Tom. I am 21 years old. I am a Worker. I have a job.";
+    }
 }
