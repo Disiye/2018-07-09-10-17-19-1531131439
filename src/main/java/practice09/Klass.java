2831e64293a5c3092c314a9bb2977de649b16ca7
WENGEM <emilio.wang@oocl.com>
2019-07-11 23:04:26 +0800

    complete Practice09Test

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,29 @@
 package practice09;
 
 public class Klass {
+
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class " + number;
+    }
+
+    public String getDisplayName() { return displayName; }
+
+    public int getNumber() { return number; }
+
+    public void assignLeader(Student student) {
+        if (number == student.getKlass().number) {
+            leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() { return leader; }
+
+    public void appendMember(Student student) { student.setKlass(this); }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,36 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() { return klass; }
+
+    @Override
+    public String introduce() {
+        String introduceString = super.introduce() + " I am a Teacher. ";
+        if(klass == null){
+            introduceString += "I teach No Class.";
+        } else{
+            introduceString += "I teach Class " + klass.getNumber() + ".";
+        }
+        return introduceString;
+    }
+
+    public String introduceWith(Student student){
+        String introduceWithString = super.introduce() + " I am a Teacher. ";
+        if(student.getKlass().getNumber() == klass.getNumber()){
+            introduceWithString += "I teach " + student.getName() + ".";
+            introduceWithString += "I don't teach " + student.getName() + ".";
+        }
+        return introduceWithString;
+    }
 }
